fba1640f90b6a8156e25fbbc2277a5cb189f6e7f
Pratik Rane <20658110+PratikRane@users.noreply.github.com>
2025-04-20 00:39:13 -0700

    Create auto_download_livestream.sh

---
+++ backend/auto_download_livestream.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# auto_download_livestream.sh
+
+CHANNEL_NAME=$1
+
+echo "Downloading livestream for channel: $CHANNEL_NAME"
+# Replace the following line with your livestream download logic
+echo "Livestream download completed for $CHANNEL_NAME"
