ac698346094d481adc8421f7cd34ea6c3637a7b2
Pratik Rane <20658110+PratikRane@users.noreply.github.com>
2025-04-20 00:38:27 -0700

    Create auto_download_shorts.sh

---
+++ backend/auto_download_shorts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# auto_download_shorts.sh
+
+CHANNEL_NAME=$1
+
+echo "Downloading shorts for channel: $CHANNEL_NAME"
+# Replace the following line with your video download logic
+echo "Shorts download completed for $CHANNEL_NAME"
