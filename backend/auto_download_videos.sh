87ba107aad5b9921865497c80c2cd6087edd3b5d
Pratik Rane <20658110+PratikRane@users.noreply.github.com>
2025-04-20 00:37:47 -0700

    Create auto_download_videos.sh

---
+++ backend/auto_download_videos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# auto_download_videos.sh
+
+CHANNEL_NAME=$1
+
+echo "Downloading videos for channel: $CHANNEL_NAME"
+# Replace the following line with your video download logic
+echo "Video download completed for $CHANNEL_NAME"
